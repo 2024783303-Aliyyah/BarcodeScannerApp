c2d9804cfe68dce6bbc65240977d103b58e8c3a1
ALIYYAH <2024783303@student.uitm.edu.my>
2026-01-07 11:58:24 +0800

    create first interface use activity_main.xml and activity_homepage.xml

--- app/src/main/java/com/example/barcodescannerapp/MainActivity.java
+++ app/src/main/java/com/example/barcodescannerapp/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.barcodescannerapp;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +26,42 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        Button getStartedButton = findViewById(R.id.button);
+
+        //nak buat bila user touch button , dia scale up
+        getStartedButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        animateButtonScale(v, 1.0f, 1.1f);
+                        return true;
+
+                    case MotionEvent.ACTION_UP:
+                        animateButtonScale(v, 1.1f, 1.0f);
+                        v.performClick();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+    public void openHomepage(View view) {
+        Intent intent = new Intent(MainActivity.this, HomepageActivity.class);
+        startActivity(intent);
+    }
+
+    private void animateButtonScale(View view, float startScale, float endScale) {
+        ObjectAnimator scaleX = ObjectAnimator.ofFloat(view, "scaleX", startScale, endScale);
+        ObjectAnimator scaleY = ObjectAnimator.ofFloat(view, "scaleY", startScale, endScale);
+
+        scaleX.setDuration(150);
+        scaleY.setDuration(150);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.play(scaleX).with(scaleY);
+        animatorSet.start();
     }
 }
